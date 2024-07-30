b09f7e3bf29fc209fe8e6319f6964086022393d4
Aradhyakapil <109958922+Aradhyakapil@users.noreply.github.com>
2024-07-30 17:29:17 +0530

    added

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectToMongo = () => {
+    mongoose.connect('mongodb://localhost:27017/project')
+        .then(() => {
+            console.log('Connected to MongoDB');
+        })
+        .catch((error) => {
+            console.error('Error connecting to MongoDB:', error);
+        });
+};
+
+module.exports = connectToMongo;
